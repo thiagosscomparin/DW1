8a45bb5e95747e36824756b55a3bea97032d3fb3
thiagosscomparin <thiago.silva.scomparin@gmail.com>
2026-03-26 11:50:50 -0300

    Add files via upload

---
+++ IMC.js
@@ -0,0 +1,37 @@
+function funcaoCalcular() {
+    let classi ="";
+    let peso = parseFloat(document.getElementById("inputPeso").value);
+    let altura = parseFloat(document.getElementById("inputAltura").value);
+    let imc = peso / (altura * altura);
+      document.getElementById("respIMC").innerHTML = imc;
+      
+       if (peso === "" || altura === "" || peso <= 0 || altura <= 0) {
+        aviso.innerHTML = "⚠️ Preencha os campos corretamente!";
+        aviso.style.display = "block";
+        return;
+    }
+
+    aviso.style.display = "none";
+
+    let footer = document.getElementById("footer");
+    let mensagem = document.getElementById("mensagem");
+    if(imc< 18.5){
+        classi= "abaixo do peso";
+    }else if(18.5<imc<25){
+        classi="Peso normal";
+    } else if(25<imc<30){
+        classi="Sobrepeso";
+    } else if(30<imc<35){
+        classi="Obesidade grau I";
+    }else if(35<imc<40){
+        classi="Obesidade grau II";
+    }else{
+        classi="Obesidade grau III";
+    }
+    document.getElementById("respClass").innerHTML = classi;
+
+    
+     
+
+
+}

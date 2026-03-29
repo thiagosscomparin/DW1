04e8911f335b21f5310dcca0b8de0aa8b96e29e0
thiagosscomparin <thiago.silva.scomparin@gmail.com>
2026-03-29 20:48:08 -0300

    Update script.js

--- 1-bimestre/IMC/script.js
+++ 1-bimestre/IMC/script.js
@@ -11,24 +11,53 @@ function funcaoCalcular() {
         return;
     }
 
+  function funcaoCalcular() {
+    let classi ="";
+    let peso = parseFloat(document.getElementById("inputPeso").value);
+    let altura = parseFloat(document.getElementById("inputAltura").value);
+    let imc = peso / (altura * altura);
+      document.getElementById("respIMC").innerHTML = imc.toFixed(2);
+      
+       if (peso === "" || altura === "" || peso <= 0 || altura <= 0) {
+        aviso.innerHTML = " Preencha os campos corretamente!";
+        aviso.style.display = "block";
+        return;
+    }
+
+   function funcaoCalcular() {
+    let classi = "";
+    let peso = parseFloat(document.getElementById("inputPeso").value);
+    let altura = parseFloat(document.getElementById("inputAltura").value);
+    let aviso = document.getElementById("aviso");
+
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        aviso.innerHTML = "Preencha os campos corretamente!";
+        aviso.style.display = "block";
+        return;
+    }
+
     aviso.style.display = "none";
 
-    let footer = document.getElementById("footer");
-    let mensagem = document.getElementById("mensagem");
-    if(imc< 18.5){
-        classi= "abaixo do peso";
-    }else if(18.5<imc<25){
-        classi="Peso normal";
-    } else if(25<imc<30){
-        classi="Sobrepeso";
-    } else if(30<imc<35){
-        classi="Obesidade grau I";
-    }else if(35<imc<40){
-        classi="Obesidade grau II";
-    }else{
-        classi="Obesidade grau III";
+    let imc = peso / (altura * altura);
+    document.getElementById("respIMC").innerHTML = imc.toFixed(2);
+
+    if (imc < 18.5) {
+        classi = "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 25) {
+        classi = "Peso normal";
+    } else if (imc >= 25 && imc < 30) {
+        classi = "Sobrepeso";
+    } else if (imc >= 30 && imc < 35) {
+        classi = "Obesidade grau I";
+    } else if (imc >= 35 && imc < 40) {
+        classi = "Obesidade grau II";
+    } else {
+        classi = "Obesidade grau III";
     }
+
     document.getElementById("respClass").innerHTML = classi;
+}
+
 
     
      

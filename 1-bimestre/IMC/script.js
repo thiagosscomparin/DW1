260b3d4fc6427f3e3000836300093ac694bf255c
thiagosscomparin <thiago.silva.scomparin@gmail.com>
2026-03-29 07:38:45 -0300

    Update script.js

--- 1-bimestre/IMC/script.js
+++ 1-bimestre/IMC/script.js
@@ -3,7 +3,7 @@ function funcaoCalcular() {
     let peso = parseFloat(document.getElementById("inputPeso").value);
     let altura = parseFloat(document.getElementById("inputAltura").value);
     let imc = peso / (altura * altura);
-      document.getElementById("respIMC").innerHTML = imc;
+      document.getElementById("respIMC").innerHTML = imc.toFixed(2);
       
        if (peso === "" || altura === "" || peso <= 0 || altura <= 0) {
         aviso.innerHTML = "⚠️ Preencha os campos corretamente!";
